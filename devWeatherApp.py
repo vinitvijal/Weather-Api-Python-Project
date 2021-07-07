bcf0e6d90787f1699c7c08e8a410d5374e1a9c1f
Vinit Vijal <vineet.shanker@soesector17rohini.co.in>
2021-07-07 22:39:19 +0530

    Done

--- devWeatherApp.py
+++ devWeatherApp.py
@@ -1,19 +1,13 @@
 import requests
 import json
 
+from requests.api import request
+
 # My Functions Are Down Here :
 def weather(city):
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=0c42f7f6b53b244c78a418f4f181282a')
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=0c42f7f6b53b244c78a418f4f181282a')    
     return response
     print()
-    # json_check = json.loads(response.text)
-    # if json_check['message'] == "city not found":
-    #     print()
-    #     print("Spelling of City is Wrong")
-    # else:
-    #     return response
-    #     print()
-        
 
 # Main Program
 
@@ -31,65 +25,73 @@ print('Username: ' + "@vinuDev_")
 print('Description: ' + "Its a Weather App Created by Vinit Vijal. ")
 print('# ' + '=' * 78)
 print()
+print("To Exit the program write : exit()")
 
 
 num = 0                     #at 0 it will directly run without asking do you want details....
 work = 1                    #Here 1 means Yes and 0 means No
 while work == 1:
-    if num != 0:
-        code = input("Try More Cities? (y/n) : ")
-        if code== "y":
+    while num == 0:
+        if num == 0:
             print()
             print()
             print()
             city = input("Enter Your City Name : ")
-            weather(city)
-            data = weather(city)
-            json_data = json.loads(data.text)
-            print()
-            # print(json_data)
-            print()
-            print()
-            print()
-            print("===========================================================")
-            print()
-            print("Name of City : " + json_data['name'])
-            print()
-            print("Current Temperature of Today is : " + str(str(round(float(json_data['main']['temp'])-273.16)))+ " C")
-            print()
-            print("Current Mood of weather : " + str(((json_data['weather'])[-1])['main']))
-            print()
-            print("===========================================================")
-            print()
-            print()
-            num += 1
-        elif code =="n" :
-            print()
-            print()
-            print()
-            print("Thanks For Using 'VinuWeather PyApp' Byy....")
-            break
-        else:
-            print("Wrong Input!!!!!")
-    elif num == 0:
-            city = input("Enter Your City Name : ")
-            weather(city)
-            data = weather(city)
-            json_data = json.loads(data.text)
-            print()
-            # print(json_data)
-            print()
-            print()
-            print()
-            print("===========================================================")
-            print()
-            print("Name of City : " + json_data['name'])
-            print()
-            print("Current Temperature of Today is : " + str(str(round(float(json_data['main']['temp'])-273.16)))+ " C")
-            print()
-            print("Current Mood of weather : " + str(((json_data['weather'])[-1])['main']))
-            print()
-            print("===========================================================")
-            print()
-            print()
-            num += 1
+            if city == "exit()":
+                print()
+                print()
+                print()
+                print("Thanks For Using 'VinuWeather PyApp' Byy....")
+                num = 5
+                work = 0
+                break
+            else : 
+                weather(city)
+                data = weather(city)
+                json_data = json.loads(data.text)
+                longitude = str(json_data['coord']['lon'])
+                latitude = str(json_data['coord']['lat'])
+                txt = 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&appid=0c42f7f6b53b244c78a418f4f181282a'
+                timezoner = requests.get(txt)
+                json_time = json.loads(timezoner.text)
+                # print(json_time)
+                print()
+                # print(json_data)
+                print()
+                print()
+                print()
+                print("===========================================================")
+                print()
+                print("Name of City : " + json_data['name'])
+                print()
+                print("Current Temperature of Today is : " + str(str(round(float(json_data['main']['temp'])-273.16)))+ " C")
+                print()
+                print("Current Mood of weather : " + str(((json_data['weather'])[-1])['main']))
+                print()
+                print("Time Zone : " + json_time['timezone'])
+                print("===========================================================")
+                print()
+                print()
+        
+    # elif num == 0:
+    #         city = input("Enter Your City Name : ")
+    #         weather(city)
+    #         data = weather(city)
+    #         json_data = json.loads(data.text)
+    #         print()
+    #         # print(json_data)
+    #         print()
+    #         print()
+    #         print()
+    #         print("===========================================================")
+    #         print()
+    #         print("Name of City : " + json_data['name'])
+    #         print()
+    #         print("Current Temperature of Today is : " + str(str(round(float(json_data['main']['temp'])-273.16)))+ " C")
+    #         print()
+    #         print("Current Mood of weather : " + str(((json_data['weather'])[-1])['main']))
+    #         print()
+    #         print("===========================================================")
+    #         print()
+    #         print()
+    #         num += 1
